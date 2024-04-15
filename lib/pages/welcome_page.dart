cab2769bdd92cfe0d5ae1f6ebe311371d90195ff
Alexander <m.mirvua@gmail.com>
2024-04-15 14:27:36 +0300

    added background image cover;

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -8,6 +8,13 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+
+  List images = [
+    "assets/images/welcome-one.png",
+    "assets/images/welcome-two.png",
+    "assets/images/welcome-three.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,11 +22,40 @@ class _WelcomePageState extends State<WelcomePage> {
       // a slider
       body: PageView.builder(
         scrollDirection: Axis.vertical,
-        itemCount: 3,
+        itemCount: images.length,
         itemBuilder: (_, index) {
           return Container(
+
+            //background image
             height: double.maxFinite,
             width: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  images[index],
+                ),
+                fit: BoxFit.cover,
+              )
+            ),
+
+            child: Row(
+              children: [
+
+                //page info
+                Column(
+                  children: [
+                    
+                  ],
+                )
+
+                //slider state
+                Column(
+                  children: [
+
+                  ],
+                )
+              ],
+            ),
 
 
           );

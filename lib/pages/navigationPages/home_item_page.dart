f5726c7c555170f791c38a155818a027e2fc70a2
Alexander <m.mirvua@gmail.com>
2024-04-16 11:26:59 +0300

    working on bottom navigation bar;

---
+++ lib/pages/navigationPages/home_item_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeItemPage extends StatefulWidget {
+  const HomeItemPage({super.key});
+
+  @override
+  State<HomeItemPage> createState() => _HomeItemPageState();
+}
+
+class _HomeItemPageState extends State<HomeItemPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
--- lib/pages/navigationPages/main_page.dart
+++ lib/pages/navigationPages/main_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_test/pages/navigationPages/bar_item_page.dart';
+import 'package:flutter_travel_test/pages/navigationPages/profile_item_page.dart';
+import 'package:flutter_travel_test/pages/navigationPages/search_item_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,10 +11,36 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
+  List bottomNavigationPages = [
+    const BarItemPage(),
+    const SearchItemPage(),
+    const ProfileItemPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.apps),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart_sharp),
+            label: "Bar",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
     );
   }
 }

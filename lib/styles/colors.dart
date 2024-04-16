c508641d9ec9b15a60148226471407365d8171c9
Alexander <m.mirvua@gmail.com>
2024-04-16 09:42:17 +0300

    added const colors to the project;

---
+++ lib/styles/colors.dart
@@ -0,0 +1,14 @@
+
+import 'dart:ui';
+
+class AppColors {
+  static const Color mainColor = Color(0xff5d69b3);
+  static const Color textColor1 = Color(0xff989acd);
+  static const Color textColor2 = Color(0xff878593);
+  static const Color bigTextColor = Color(0xff2e2e31);
+  static const Color starColor = Color(0xffe7be4b);
+  static const Color mainTextColor = Color(0xffababad);
+  static const Color buttonBackgroundColor = Color(0xfff1f2f9);
+
+
+}

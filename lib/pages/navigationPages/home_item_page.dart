74ed31a8e77c32e7586061162955975a3f3124b7
Alexander <m.mirvua@gmail.com>
2024-04-16 12:10:59 +0300

    finished top bar in main page;

--- lib/pages/navigationPages/home_item_page.dart
+++ lib/pages/navigationPages/home_item_page.dart
@@ -10,7 +10,36 @@ class HomeItemPage extends StatefulWidget {
 class _HomeItemPageState extends State<HomeItemPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+
+          //top bar
+          Container(
+            padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(Icons.menu, size: 30, color: Colors.black,),
+
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage('assets/images/moon-profile.png'),
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(.5),
+                  ),
+                ),
+
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 

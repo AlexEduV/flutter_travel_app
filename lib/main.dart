dcdc8fcc33540f80a30ba478348c9660588a86bc
Alexander <m.mirvua@gmail.com>
2024-04-16 11:36:08 +0300

    working on bottom navigation bar;

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_test/pages/navigationPages/main_page.dart';
 import 'package:flutter_travel_test/pages/welcome_page.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const WelcomePage(),
+      home: const MainPage(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/pages/navigationPages/main_page.dart
+++ lib/pages/navigationPages/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_travel_test/pages/navigationPages/bar_item_page.dart';
+import 'package:flutter_travel_test/pages/navigationPages/home_item_page.dart';
 import 'package:flutter_travel_test/pages/navigationPages/profile_item_page.dart';
 import 'package:flutter_travel_test/pages/navigationPages/search_item_page.dart';
 
@@ -13,15 +14,32 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
 
   List bottomNavigationPages = [
+    const HomeItemPage(),
     const BarItemPage(),
     const SearchItemPage(),
     const ProfileItemPage(),
   ];
 
+  int _selectedIndex = 0;
+
+  void onBottomNavigationitemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: bottomNavigationPages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+        onTap: onBottomNavigationitemTap,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey.withOpacity(.5),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        elevation: 0,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.apps),

819b3a54b4016ccdc953aaa5e6d6abf0639de8a6
Alexander <m.mirvua@gmail.com>
2024-04-16 11:14:14 +0300

    working on a bottom navigation pages;

---
+++ lib/pages/navigationPages/profile_item_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class ProfileItemPage extends StatelessWidget {
+  const ProfileItemPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const Center(
+      child: Text('Profile Item Page'),
+    );
+  }
+}
---
+++ lib/pages/navigationPages/search_item_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class SearchItemPage extends StatelessWidget {
+  const SearchItemPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const Center(
+      child: Text('Search Item Page'),
+    );
+  }
+}

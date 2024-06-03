3ca9de230848b5e98f53195173b003b9709b1e55
Alexander <m.mirvua@gmail.com>
2024-06-03 18:19:02 +0300

    connected details page to the data model;

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -9,6 +9,9 @@ import 'package:flutter_travel_test/widgets/app_text.dart';
 import 'package:flutter_travel_test/widgets/responsive_button.dart';
 import 'package:gap/gap.dart';
 
+import '../cubit/app_cubit_states.dart';
+import '../model/data_model.dart';
+
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
 
@@ -17,8 +20,6 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
-
-  final int stars = 3;
 
   late int selectedPeopleIndex = -1;
 
@@ -29,219 +30,227 @@ class _DetailPageState extends State<DetailPage> {
       onPopInvoked: (bool isInvoked) {
         BlocProvider.of<AppCubits>(context).goHome();
       },
-      child: Scaffold(
-        body: SizedBox(
-          width: double.maxFinite,
-          height: double.maxFinite,
-          child: Stack(
-            children: [
+      child: BlocBuilder<AppCubits, CubitStates>(
+        builder: (context, state) {
 
-              //background image
-              Positioned(
-                  child: Container(
-                    height: 350,
-                    width: double.maxFinite,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/mountain.jpeg"),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  )
-              ),
+          DetailState detailState = state as DetailState;
+          DataModel place = detailState.place;
 
-              //top bar
-              Positioned(
-                left: 20,
-                top: 50,
-                right: 20,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //menu button
-                    IconButton(
-                      onPressed: (){
-                        BlocProvider.of<AppCubits>(context).goHome();
-                      },
-                      icon: const Icon(
-                        Icons.chevron_left,
-                        color: Colors.white,
-                      ),
-                    ),
+          return Scaffold(
+            body: SizedBox(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              child: Stack(
+                children: [
 
-                    //more button
-                    IconButton(
-                      onPressed: (){},
-                      icon: const Icon(
-                        Icons.more_vert,
-                        color: Colors.white,
-                      ),
-                    ),
-
-                  ],
-                ),
-              ),
-
-              //details
-              Positioned(
-                top: 320,
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 500,
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30.0),
-                      topRight: Radius.circular(30.0),
-                    ),
-                    color: Colors.white,
+                  //background image
+                  Positioned(
+                      child: Container(
+                        height: 350,
+                        width: double.maxFinite,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage('http://mark.bslmeiyu.com/uploads/${place.imgSource}',),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      )
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
 
-                      const Gap(25.0),
-
-                      //name and price
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          AppLargeText(
-                            text: 'Yosemite',
-                            color: Colors.black87,
-                            fontSize: 28,
+                  //top bar
+                  Positioned(
+                    left: 20,
+                    top: 50,
+                    right: 20,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        //menu button
+                        IconButton(
+                          onPressed: (){
+                            BlocProvider.of<AppCubits>(context).goHome();
+                          },
+                          icon: const Icon(
+                            Icons.chevron_left,
+                            color: Colors.white,
                           ),
+                        ),
 
-                          AppLargeText(
-                            text: '\$ 250',
-                            color: AppColors.mainColor,
-                            fontSize: 28,
+                        //more button
+                        IconButton(
+                          onPressed: (){},
+                          icon: const Icon(
+                            Icons.more_vert,
+                            color: Colors.white,
                           ),
-                        ],
+                        ),
+
+                      ],
+                    ),
+                  ),
+
+                  //details
+                  Positioned(
+                    top: 320,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 500,
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30.0),
+                          topRight: Radius.circular(30.0),
+                        ),
+                        color: Colors.white,
                       ),
-
-                     const Gap(10.0),
-
-                      //location (icon and text)
-                      const Row(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Icon(Icons.location_pin, color: AppColors.mainColor,),
 
-                          Gap(5.0),
+                          const Gap(25.0),
 
-                          AppText(
-                            text: 'USA, California',
-                            color: AppColors.mainColor,
-                            fontSize: 14.0,
+                          //name and price
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              AppLargeText(
+                                text: place.name,
+                                color: Colors.black87,
+                                fontSize: 28,
+                              ),
+
+                              AppLargeText(
+                                text: '\$ ${place.price}',
+                                color: AppColors.mainColor,
+                                fontSize: 28,
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
 
-                      const Gap(10.0),
+                         const Gap(10.0),
 
-                      //stars
-                      Row(
-                        children: [
-                          Wrap(
-                            children:
-                              List.generate(5, (index) {
-                                return Icon(
-                                  index < stars ? Icons.star: Icons.star_border,
-                                  color: index < stars ? AppColors.starColor : AppColors.textColor2,
-                                );
-                              })
+                          //location (icon and text)
+                          Row(
+                            children: [
+                              const Icon(Icons.location_pin, color: AppColors.mainColor,),
+
+                              const Gap(5.0),
+
+                              AppText(
+                                text: place.location,
+                                color: AppColors.mainColor,
+                                fontSize: 14.0,
+                              ),
+                            ],
                           ),
 
                           const Gap(10.0),
 
-                          AppText(
-                            text: '($stars.0)',
-                            color: AppColors.textColor2,
+                          //stars
+                          Row(
+                            children: [
+                              Wrap(
+                                children:
+                                  List.generate(5, (index) {
+                                    return Icon(
+                                      index < place.stars ? Icons.star: Icons.star_border,
+                                      color: index < place.stars ? AppColors.starColor : AppColors.textColor2,
+                                    );
+                                  })
+                              ),
+
+                              const Gap(10.0),
+
+                              AppText(
+                                text: '(${place.stars}.0)',
+                                color: AppColors.textColor2,
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
 
-                      const Gap(35.0),
+                          const Gap(35.0),
 
-                      const AppLargeText(
-                        text: 'People',
-                        color: Colors.black87,
-                        fontSize: 20,
-                      ),
-
-                      const Gap(5.0),
-
-                      const AppText(
-                        text: 'Number of people in your group',
-                        color: AppColors.mainTextColor,
-                      ),
-
-                      const Gap(10.0),
-
-                      Wrap(
-                        spacing: 8.0,
-                        children: List.generate(5, (index) {
-
-                          return InkWell(
-                            onTap: () {
-                              setState(() {
-                                selectedPeopleIndex = index;
-                              });
-                            },
-                            child: AppButton(
-                              text: '${index + 1}',
-                              backgroundColor: index == selectedPeopleIndex ? Colors.black87 : AppColors.buttonBackgroundColor,
-                              color: index == selectedPeopleIndex ? AppColors.buttonBackgroundColor : Colors.black87,
-                              borderColor: index == selectedPeopleIndex ? Colors.black87 : AppColors.buttonBackgroundColor,
-                              size: 55,
-                            ),
-                          );
-
-                        }),
-                      ),
-
-                      const Gap(35.0),
-
-                      const AppLargeText(text: 'Description', fontSize: 20,),
-
-                      const Gap(5.0),
-
-                      const AppText(
-                        text: 'Yosemite National Park is located in central Sierra Nevada in the US state of California. It is located near the wild protected areas.',
-                        color: AppColors.mainTextColor,
-                      ),
-
-                      const Gap(20.0),
-
-                      const Row(
-                        children: [
-
-                          AppButton(
-                            backgroundColor: Colors.white,
+                          const AppLargeText(
+                            text: 'People',
                             color: Colors.black87,
-                            borderColor: Colors.black87,
-                            icon: CupertinoIcons.heart,
+                            fontSize: 20,
                           ),
 
-                          Gap(10.0),
+                          const Gap(5.0),
 
-                          Expanded(
-                            child: ResponsiveButton(
-                              text: 'Book Trip Now',
-                            ),
+                          const AppText(
+                            text: 'Number of people in your group',
+                            color: AppColors.mainTextColor,
                           ),
+
+                          const Gap(10.0),
+
+                          Wrap(
+                            spacing: 8.0,
+                            children: List.generate(5, (index) {
+
+                              return InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    selectedPeopleIndex = index;
+                                  });
+                                },
+                                child: AppButton(
+                                  text: '${index + 1}',
+                                  backgroundColor: index == selectedPeopleIndex ? Colors.black87 : AppColors.buttonBackgroundColor,
+                                  color: index == selectedPeopleIndex ? AppColors.buttonBackgroundColor : Colors.black87,
+                                  borderColor: index == selectedPeopleIndex ? Colors.black87 : AppColors.buttonBackgroundColor,
+                                  size: 55,
+                                ),
+                              );
+
+                            }),
+                          ),
+
+                          const Gap(35.0),
+
+                          const AppLargeText(text: 'Description', fontSize: 20,),
+
+                          const Gap(5.0),
+
+                          AppText(
+                            text: place.description,
+                            color: AppColors.mainTextColor,
+                          ),
+
+                          const Gap(20.0),
+
+                          const Row(
+                            children: [
+
+                              AppButton(
+                                backgroundColor: Colors.white,
+                                color: Colors.black87,
+                                borderColor: Colors.black87,
+                                icon: CupertinoIcons.heart,
+                              ),
+
+                              Gap(10.0),
+
+                              Expanded(
+                                child: ResponsiveButton(
+                                  text: 'Book Trip Now',
+                                ),
+                              ),
+
+                            ],
+                          )
 
                         ],
-                      )
+                      ),
+                    ),
+                  )
 
-                    ],
-                  ),
-                ),
-              )
-
-            ],
-          ),
-        ),
+                ],
+              ),
+            ),
+          );
+        }
       ),
     );
   }
